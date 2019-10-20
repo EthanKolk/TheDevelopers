aa7ddd4fd543fd8a44be551827faf81a9c9a2ded
EthanKolk <55165790+EthanKolk@users.noreply.github.com>
2019-10-19 21:17:30 -0500

    Login Prototype
    Created Login prototype.
    (No Google sign-in)

--- app/src/main/java/com/example/restrauntfinder/MainActivity.java
+++ app/src/main/java/com/example/restrauntfinder/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.restrauntfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Go to next activitiy
+    public void LogIn(View view) {
+        //Check to see if the password is good then
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
+        //Else some fail message
     }
 }
