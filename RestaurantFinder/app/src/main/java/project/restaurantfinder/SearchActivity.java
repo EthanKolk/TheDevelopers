19f2251c899930873d621dc01d089afbd25ce5bb
James <jamesdurflinger@gmail.com>
2019-11-05 19:43:21 -0600

    Update Search Activity. Can't resolve AutoCompleteSupportFragment.

--- RestaurantFinder/app/src/main/java/project/restaurantfinder/SearchActivity.java
+++ RestaurantFinder/app/src/main/java/project/restaurantfinder/SearchActivity.java
@@ -2,15 +2,31 @@ package project.restaurantfinder;
 
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    PlacesClient pc;
+    List<Place.Field> pFields = Arrays.asList(Place.Field.ID,
+            Place.Field.NAME, Place.Field.ADDRESS);
+    AutocompleteSupportFragment pFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,10 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initPlaces();
+
+        setupAutoComplete();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,4 +49,27 @@ public class SearchActivity extends AppCompatActivity {
         });
     }
 
+    private void setupAutoComplete()
+    {
+        pFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.places_autocomplete_fragment);
+        pFragment.setPlaceFields(pFields);
+        pFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                Toast.makeText(SearchActivity.this, place.getName(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Toast.makeText(SearchActivity.this, ""+status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initPlaces()
+    {
+        String apiKey="AIzaSyCTyRlS0MCx4cQ1jw71jMi_SUcapo_vlg8";
+        Places.initialize(this, apiKey);
+        pc = Places.createClient(this);
+    }
 }
