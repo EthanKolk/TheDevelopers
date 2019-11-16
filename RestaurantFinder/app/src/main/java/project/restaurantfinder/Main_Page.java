a55e0590687e578f5a9a31f5de9edf89c960a327
James <jamesdurflinger@gmail.com>
2019-11-15 22:56:20 -0600

    Fab button makes you go to map

--- RestaurantFinder/app/src/main/java/project/restaurantfinder/Main_Page.java
+++ RestaurantFinder/app/src/main/java/project/restaurantfinder/Main_Page.java
@@ -318,8 +318,12 @@ public class Main_Page extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                 //       .setAction("Action", null).show();
+
+                //Go To Map
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
             }
         });
 
